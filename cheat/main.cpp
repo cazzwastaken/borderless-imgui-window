732dd5a5f227ab2a7b2c7cbe714f09b99ec866e3
cazzwastaken <86018767+cazzwastaken@users.noreply.github.com>
2021-11-28 18:30:08 +0200

    Initial code commit

---
+++ cheat/main.cpp
@@ -0,0 +1,31 @@
+#include "gui.h"
+
+#include <thread>
+
+int __stdcall wWinMain(
+	HINSTANCE instance,
+	HINSTANCE previousInstance,
+	PWSTR arguments,
+	int commandShow)
+{
+	// create gui
+	gui::CreateHWindow("Cheat Menu", "Cheat Menu Class");
+	gui::CreateDevice();
+	gui::CreateImGui();
+
+	while (gui::exit)
+	{
+		gui::BeginRender();
+		gui::Render();
+		gui::EndRender();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	// destroy gui
+	gui::DestroyImGui();
+	gui::DestroyDevice();
+	gui::DestroyHWindow();
+
+	return EXIT_SUCCESS;
+}
