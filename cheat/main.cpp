f7bafdeadced86d3052d73c9cd1dfca80b6b9fab
cazzwastaken <cazzwastaken@gmail.com>
2021-12-23 20:47:08 +0200

    lowered sleep for more fps

--- cheat/main.cpp
+++ cheat/main.cpp
@@ -19,7 +19,7 @@ int __stdcall wWinMain(
 		gui::Render();
 		gui::EndRender();
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
 	}
 
 	// destroy gui
